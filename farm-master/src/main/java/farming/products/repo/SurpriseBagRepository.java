4a3c52482f6d5cba99c46fbd6ddb1368dfe2bf3e
oksana borko <ksenya.borko@gmail.com>
2025-03-12 09:19:03 +0200

    debug

--- farm-master/src/main/java/farming/products/repo/SurpriseBagRepository.java
+++
@@ -1,9 +0,0 @@
-package farming.products.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import farming.products.entity.SurpriseBag;
-
-public interface SurpriseBagRepository extends JpaRepository<SurpriseBag, Long>{
-
-}
